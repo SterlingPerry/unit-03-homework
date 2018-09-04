fd097c3e6117048917f159862a763c949837585f
SterlingPerry <sterling.perry@georgiacourts.gov>
2018-09-04 17:09:33 -0400

    initial commit

---
+++ unsolved/app.js
@@ -0,0 +1,78 @@
+const employeeList = [
+  {
+    name: 'Jan',
+    officeNum: 123,
+    phoneNum: '222-222-2222'
+  },
+  {
+    name: 'Juan',
+    officeNum: 304,
+    phoneNum: '489-789-8789'
+  },
+  {
+    name: 'Margie',
+    officeNum: 789,
+    phoneNum: '789-789-7897'
+  },
+  {
+    name: 'Sara',
+    officeNum: 32,
+    phoneNum: '222-789-4654'
+  },
+  {
+    name: 'Tyrell',
+    officeNum: 345,
+    phoneNum: '566-621-0452'
+  },
+  {
+    name: 'Tasha',
+    officeNum: 213,
+    phoneNum: '789-766-5675'
+  },
+  ,
+  {
+    name: 'Ty',
+    officeNum: 211,
+    phoneNum: '789-766-7865'
+  },
+  {
+    name: 'Sarah',
+    officeNum: 345,
+    phoneNum: '222-789-5231'
+  }
+];
+
+const commandPrint = prompt("enter a command here");
+
+  for (let i = 0; i < employeeList.length; i++) {
+    if (commandPrint === print) {
+    render(employeeList[i].name);
+    render(employeeList[i].officeNum);
+    render(employeeList[i].phoneNum);
+  }
+
+
+}
+
+const commandVerify = prompt("enter a command here");
+const commandVerifyName = prompt("enter a name here");
+
+  for (let i = 0; i < employeeList.length; i++) {
+    if (commandVerify === verify) {
+      message = 'true'
+  }
+
+}
+
+const commandLookup = prompt("enter a command here");
+const commandLookupName = prompt("enter a name here");
+const commandLookupName = employeeList[i]
+ 
+      for (let i = 0; i < employeeList.length; i++) {
+        if (commandLookup === lookup) {
+        render(employeeList[i].name);
+        render(employeeList[i].officeNum);
+        render(employeeList[i].phoneNum);
+  }
+
+}
